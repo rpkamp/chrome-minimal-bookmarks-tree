568093e92e08671630f073e9108a504a230fced3
Remon van de Kamp <rpkamp@gmail.com>
2012-05-27 13:05:57 +0200

    When holding control and pressing a bookmark, open bookmark in background tab and keep MBT open

--- js/popup.js
+++ js/popup.js
@@ -21,10 +21,14 @@ chrome.bookmarks.getTree(function(x) {
 				} else {
 						var url = $this.data('url');
 						if (e.button == 0) {
-								chrome.tabs.getSelected(null, function(tab) {
-										chrome.tabs.update(tab.id, {url: url});
-										window.close();
-								});
+								if (e.ctrlKey) {
+										chrome.tabs.create({url: url, active: false});
+								} else {
+										chrome.tabs.getSelected(null, function(tab) {
+												chrome.tabs.update(tab.id, {url: url});
+												window.close();
+										});
+								}
 						} else if (e.button == 1) {
 								chrome.tabs.create({url: url});
 						} else if (e.button == 2) {
