7d67f37b85fcae50108a929226b879460ca82876
Remon van de Kamp <rpkamp@gmail.com>
2016-05-15 13:55:06 +0200

    Try another way to get the latest Chrome on Travis

--- mbt.conf.js
+++ mbt.conf.js
@@ -56,8 +56,15 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
@@ -65,6 +72,10 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
+  }
+
+  if (process.env.TRAVIS) {
+    cfg.browsers = ['Chrome_travis_ci'];
   }
 
   config.set(cfg)
