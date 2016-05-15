77b097f7b238366b358f879025f05600e2a7be35
Remon van de Kamp <rpkamp@gmail.com>
2016-05-15 12:53:04 +0200

    Another attempt to get Chrome to work under Travis

--- mbt.conf.js
+++ mbt.conf.js
@@ -58,6 +58,13 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -67,4 +74,8 @@ module.exports = function(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   })
+
+  if (process.env.TRAVIS) {
+    cfg.browsers = ['Chrome_travis_ci'];
+  }
 }
