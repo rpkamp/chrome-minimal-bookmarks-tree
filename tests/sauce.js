11d0c8da5c102c0bc71d21058531966327695ff8
Remon van de Kamp <rpkamp@gmail.com>
2017-01-25 22:59:20 +0100

    Update qUnit, load qUnit from jQuery CDN

--- tests/sauce.js
+++ tests/sauce.js
@@ -1,21 +1,26 @@
 var log = [];
-QUnit.done = function (test_results) {
-  test_results.tests = log.map(function (details) {
-    return {
+
+QUnit.done(function (test_results) {
+  var tests = [];
+  for(var i = 0, len = log.length; i < len; i++) {
+    var details = log[i];
+    tests.push({
       name: details.name,
       result: details.result,
       expected: details.expected,
       actual: details.actual,
       source: details.source
+    });
+  }
+  test_results.tests = tests;
+
+  window.global_test_results = test_results;
+});
+QUnit.testStart(function(testDetails){
+  QUnit.log(function(details){
+    if (!details.result) {
+      details.name = testDetails.name;
+      log.push(details);
     }
   });
-
-  // delaying results a bit cause in real-world
-  // scenario you won't get them immediately
-  setTimeout(function () {
-    window.global_test_results = test_results;
-  }, 2000);
-};
-QUnit.log(function (details) {
-  log.push(details);
 });
