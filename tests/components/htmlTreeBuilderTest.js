041f838b238fe055bc3e067af343ef3965c928b4
Remon van de Kamp <rpkamp@gmail.com>
2017-01-25 22:36:12 +0100

    Remove no longer needed test

--- tests/components/htmlTreeBuilderTest.js
+++
@@ -1,8 +0,0 @@
-QUnit.test("Build Simple Node", function (assert) {
-  var builder = new HtmlTreeBuilder();
-  let node = builder.buildTreeNode({
-    id: 1,
-    name: 'Foo'
-  });
-  assert.equal(node.html(), '<ul><li data-id="1">Foo</li></ul>');
-});
