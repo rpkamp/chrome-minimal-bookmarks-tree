2097a58d7c184126bb080cbce6b286c7e06cf4da
Remon van de Kamp <rpkamp@gmail.com>
2019-06-23 20:42:05 +0200

    Switch from Grunt/webpack to Symfony Encore
    It's way easier to use for processing SASS files

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+  .disableSingleRuntimeChunk()
+  .enableVersioning(false)
+  .configureManifestPlugin((options) => {
+    options.fileName = 'webpack-manifest.json';
+  })
+
+  .setOutputPath('dist/')
+  .setPublicPath('/')
+
+  .addStyleEntry('browser_action_css', './src/browser_action/main.scss')
+  .addStyleEntry('options_css', './src/options/main.scss')
+
+  .addEntry('browser_action', './src/browser_action/index.js')
+  .addEntry('background', './src/background/index.js')
+  .addEntry('options', './src/options/index.js')
+
+  .copyFiles([
+    {
+      from: './src/icons',
+      to: 'icons/[path][name].[ext]'
+    },
+    {
+      from: './src/_locales',
+      context: 'src/'
+    },
+    {
+      from: './src/',
+      pattern: /\.json$/,
+      includeSubdirectories: false
+    },
+    {
+      from: './src/background',
+      pattern: /\.html$/,
+      includeSubdirectories: false
+    },
+    {
+      from: './src/browser_action',
+      pattern: /\.html$/,
+      includeSubdirectories: false
+    },
+    {
+      from: './src/options',
+      pattern: /\.html$/,
+      includeSubdirectories: false
+    }
+  ])
+
+  .enableSassLoader()
+  .enableEslintLoader()
+
+  .enableSourceMaps(!Encore.isProduction())
+  .cleanupOutputBeforeBuild()
+  .configureFilenames({
+    js: '[name].js',
+    css: '[name].css'
+  })
+;
+
+module.exports = Encore.getWebpackConfig();
